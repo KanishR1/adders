5241462794692cf94c0a35463ccc686b09d9ed67
Kanish <kanishjarvis@gmail.com>
2023-10-04 22:44:46 +0530

    Updated

--- Ripple Carry Adder/rca.v
+++ Ripple Carry Adder/rca.v
@@ -1,12 +1,16 @@
 `include "fa.v"
-module rca(
-    input [3:0] A, input [3:0] B, input Cin, output [3:0] S, output Cout
+module rca #(parameter INPUT_SIZE = 16 )(
+    input [INPUT_SIZE-1:0] A, input [INPUT_SIZE-1:0] B, input Cin, output [INPUT_SIZE-1:0] S
 );
-    wire [2:0]C;
-
-    fa M1(.A(A[0]), .B(B[0]), .Cin(Cin), .S(S[0]), .C(C[0]));
-    fa M2(.A(A[1]), .B(B[1]), .Cin(C[0]), .S(S[1]), .C(C[1]));
-    fa M3(.A(A[2]), .B(B[2]), .Cin(C[1]), .S(S[2]), .C(C[2]));
-    fa M4(.A(A[3]), .B(B[3]), .Cin(C[2]), .S(S[3]), .C(Cout));
+    wire [INPUT_SIZE:0]c;
+    assign c[0] = Cin;
+    wire [INPUT_SIZE-1:0]sum;
+    genvar i;
+    generate 
+    for(i=0;i<INPUT_SIZE;i=i+1) begin : ripple
+    	fa M1(.A(A[i]), .B(B[i]), .Cin(c[i]), .S(sum[i]), .C(c[i+1]));
+    end
+    endgenerate
+    assign S = c[16] ? {c[i+1],sum} : sum;
 
 endmodule
